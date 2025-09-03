800830e45e7b0839a8c718b813c9a7ed912f31a7
brenoacf <brenoacf@gmail.com>
2025-09-03 00:09:43 -0300

    feat: add after read customer logic

---
+++ srv/main.ts
@@ -0,0 +1,13 @@
+import { Service } from '@sap/cds';
+import { Customers } from '@models/sales';
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers) => {
+        results.forEach(customer => {
+            if (!customer.email?.includes('@')) {
+                customer.email = `${customer.email}@gmail.com`;
+            }
+        })
+    })
+
+}
